3b1cce280fbfd1d062e6880d133a29400a2d1376
MadisOllikainen <madis_ollikainen@hotmail.com>
2016-07-09 12:55:48 +0200

    Added an helper function add_leaf_(std::string) to the MerkelHasher class. This function will update the leafs_ vector as well as update the roots_ vector in such a way that the 'forest of complete trees' (which will later be merged into on big 'canonical' Merkel tree) can be built without knowing the final number of leafs.

--- include/merkelHasher.hpp
+++ include/merkelHasher.hpp
@@ -26,13 +26,31 @@
 
 #include <vector>
 #include <string>
+#include <algorithm>
 
+
+/*****************************************************
+ *** The templated MerkelHasher class declaration. ***
+ *****************************************************/
 
  template <std::string (*H)(const std::string), std::string (*M)(const std::string, const std::string )>
  class MerkelHasher
  {
 
  public:
+
+ 	/*
+ 	 * Simple consturctor adding the initial empty
+ 	 * string to the roots_ vector. The placement of 
+ 	 * empty strings in the roots_ vector will be used
+ 	 * as information carriers during the construction of
+ 	 * the 'canonical' Merkel Tree. 
+ 	 *
+ 	 * No specific destructor must be declared, as only STL 
+ 	 * container have been used, which take care of destrying themselves.
+ 	 *
+ 	 */
+ 	MerkelHasher(){ roots_.push_back("");}
 
 
  	/*
@@ -42,6 +60,20 @@
  	std::string hash(const std::string s){	return H(s);	}
  	std::string hash(const std::string s1, const std::string s2){	return H( M(s1,s2) );	}
 
+ 	void test(const std::string leaf)
+ 	{
+ 		add_leaf_(leaf);
+
+ 		std::cout << "Added a new leaf: " << leaf << std::endl;
+ 		std::cout << "leafs_ vector: ";
+ 		for (uint i=0; i<leafs_.size(); ++i) { std::cout << leafs_[i] << " "; }
+ 		std::cout << std::endl << "roots_ vector: ";
+ 		for (uint i=0; i<roots_.size(); ++i) 
+ 		{ 	if(roots_[i]=="") std::cout << "-" << " ";
+ 			else std::cout << roots_[i] << " ";  
+ 		}
+ 		std::cout<<std::endl;
+ 	}
 
 
  private:
@@ -49,8 +81,68 @@
  	std::vector<std::string> leafs_;	// Vector to hold the leaf hashes
  	std::vector<std::string> roots_;	
 
+ 	/*
+ 	 * A helper function for adding a single leaf 
+ 	 * into the tree. The function body will be 
+ 	 * 
+ 	 */
+ 	 void add_leaf_(std::string leaf);
 
- };
+ }; // END MERKELHASHER DECLARATION
+
+
+
+/*****************************************************
+ *** The MerkelHasher member function definitions. ***
+ *****************************************************/
+
+ template <std::string (*H)(const std::string), std::string (*M)(const std::string, const std::string )>
+ void MerkelHasher<H,M>::add_leaf_(std::string leaf)
+ {
+ 	// Add the new leaf to the leafs vector
+ 	leafs_.push_back(leaf);
+
+ 	// Traverse the roots_ vector and update it.
+ 	// Traversal is done with simple rules:
+ 	//
+ 	//		a)	If the roots_[i] is an empty string,
+ 	//			set it to be the current 'leaf' value
+ 	//			roots_[i] = leaf. Finish the loop.
+ 	//
+ 	// 		b)	If roots_[i] is a non-empty string,
+ 	//		 	then hash it together with the leaf
+ 	//			leaf = hash(roots_[i],leaf) and set 
+ 	//			change it to empty string roots_[i]="". 
+ 	//			After that carry on with the loop.
+ 	//		
+ 	//		c)	If the end of the vector roots_ is reach
+ 	//			without finding an empty roots_[i], then 
+ 	//			push the hashed together leaf to the end 
+ 	//			end of the roots_ vector.
+ 	//			   
+ 	//
+ 	for (uint i=0; i<roots_.size(); ++i)
+ 	{
+ 		if( roots_[i] == "" )
+ 		{
+ 			// roots_[i] = leaf;
+ 			std::swap(roots_[i],leaf);
+ 			break;
+ 		}
+ 		else
+ 		{
+ 			leaf = hash(roots_[i],leaf);
+ 			roots_[i] = "";
+ 		}
+ 	}
+ 	if(leaf != "" )
+ 	{
+ 		roots_.push_back(leaf);
+ 		leaf = "";
+ 	}
+
+ }
+
 
 
 #endif // MERKEL_HASHER_HPP
--- src/test.cpp
+++ src/test.cpp
@@ -4,26 +4,38 @@
 #include "myHashInterface.hpp"
 #include "merkelHasher.hpp"
 
-
+std::string self_hash(const std::string str)
+{
+	return str;
+}
 
 int main( int argc, char **argv )
 {
 
 
-	MerkelHasher<sha256,myHashMerge> myHasher;
+	// MerkelHasher<sha256,myHashMerge> myHasher;
+	MerkelHasher<self_hash,myHashMerge> myHasher;
 
-	const std::string s1("LogTest1");
-	const std::string s2("LogTest2");
+	// const std::string s1("LogTest1");
+	// const std::string s2("LogTest2");
 	
-	std::string h1 = myHasher.hash(s1);
-	std::string h2 = myHasher.hash(s2);
-	std::string h3 = myHasher.hash(h1,h2);
+	// std::string h1 = myHasher.hash(s1);
+	// std::string h2 = myHasher.hash(s2);
+	// std::string h3 = myHasher.hash(h1,h2);
 
 
 
-	std::cout << "hash(" << s1 << "):\t" << h1 << std::endl;
-	std::cout << "hash(" << s2 << "):\t" << h2 << std::endl;
-	std::cout << "hash( hash(" << s1 << ")+hash(" << s2 << ") ):\t" << h3 << std::endl;
+	// std::cout << "hash(" << s1 << "):\t" << h1 << std::endl;
+	// std::cout << "hash(" << s2 << "):\t" << h2 << std::endl;
+	// std::cout << "hash( hash(" << s1 << ")+hash(" << s2 << ") ):\t" << h3 << std::endl;
+
+	// std::cout<<std::endl;
+	std::vector<std::string> vs{"1","2","3","4", "5", "6", "7"};
+	for( uint i =0; i<vs.size(); ++i ) 
+	{ 
+		myHasher.test(vs[i]); 
+	}
+		
 
 	return 0 ;
 
