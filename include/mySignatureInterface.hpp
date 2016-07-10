bca6206c8d552fb2e35f17361dcd733aa7dc8ed8
MadisOllikainen <madis_ollikainen@hotmail.com>
2016-07-10 13:11:37 +0200

    Added include/mySignatureInterface.hpp, which implements an empty signature function. Empty meaning, that it just returns the same string as inputed.

---
+++ include/mySignatureInterface.hpp
@@ -0,0 +1,14 @@
+#ifndef MY_SIGNATURE_INTERFACE_HPP
+#define MY_SIGNATURE_INTERFACE_HPP
+
+#include <string>
+
+// An empty signature function, which just
+// returns the same string again.
+
+std::string signature(std::string& str)
+{
+	return str;
+}
+
+#endif // MY_SIGNATURE_INTERFACE_HPP
