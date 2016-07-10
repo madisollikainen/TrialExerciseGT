e0f57f951530cdde98b5987e9de312b704f19eec
MadisOllikainen <madis_ollikainen@hotmail.com>
2016-07-10 21:43:34 +0200

    Added short bash script for running the test_hasher tool on a 'log file' numbers.log, which has numbers 0 to 9 on its lines. As the test_hasher uses identity function as a hash function, the outputs of these test runs should be easilly manually controllable.

---
+++ run_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This is a short script for running 
+# the test_hasher tool on the numers.log
+# 'log file'. As the test_hasher is 
+# mostly the same code as hasher, but 
+# with just the difference of using an
+# identity hash function, it will enable 
+# better checking of the code output. 
+# The numbers.log file has 10 lines, with 
+# values from 0 to 9 on the lines. 
+
+# Run with signing and leaves storing
+./build/test_hasher -i example_logs/numbers.log --leaves
+
+# Run with hash chain extraction for all lines
+cp example_logs/numbers.log  example_logs/tmp_lines_for_numbers
+./build/test_hasher -i example_logs/numbers.log --chain example_logs/tmp_lines_for_numbers
+rm example_logs/tmp_lines_for_numbers
+
+# Move the output to test_output directory
+mkdir -p test_output
+mv example_logs/numbers.log.* test_output/
+
+# Echo that the script has completed
+echo 'Completed running tests'
